d415f631670d950b53c587b92833e397896308bc
Riccardo Venturini <riccardoventurini88@hotmail.com>
2018-06-02 08:35:07 +0100

    navigation bar added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    <nuxt/>
+    <TheHeader />
+    <main class="main-content">
+      <nuxt/>
+    </main>
   </div>
 </template>
 
-<style>
-
-h1 {
-  color: blue;
+<script>
+import TheHeader from '@/components/TheHeader/TheHeader'
+export default {
+    components: {
+      TheHeader
+    }
 }
 
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Lato", sans-serif;
+  }
+  .main-content {
+    margin-top: 4.5rem;
+  }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: "Vue the future",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: "A Blog built with Storyblok and Nuxt" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:400,700'}
     ]
   },
 
